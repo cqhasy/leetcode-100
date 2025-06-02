240d9b453d1e93d5ed9a76b088c2ac5e2f7c8ab4
cqhasy <3181940782@qq.com>
2025-06-02 21:41:32 +0800

    two

---
+++ groupAnagrams.go
@@ -0,0 +1,43 @@
+package leetcode_100
+
+func groupAnagrams(strs []string) [][]string {
+	var res [][]string
+	m := make(map[string]int)
+	var x = 0
+	if len(strs) == 0 {
+		return [][]string{}
+
+	}
+	for k, v := range strs {
+		if k == 0 {
+			res = append(res, []string{v})
+			m[sortStr(v)] = x
+			x++
+			continue
+		}
+		val, ok := m[sortStr(v)]
+		if !ok {
+			res = append(res, []string{v})
+			m[sortStr(v)] = x
+			x++
+			continue
+		}
+		res[val] = append(res[val], v)
+
+	}
+	return res
+}
+func sortStr(s string) string {
+	b := []byte(s)
+	var m byte
+	for i := 0; i < len(b); i++ {
+		for j := i + 1; j < len(b); j++ {
+			if b[i] > b[j] {
+				m = b[i]
+				b[i] = b[j]
+				b[j] = m
+			}
+		}
+	}
+	return string(b)
+}
